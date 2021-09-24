56f04f4a828a17933e8fa9c5fd91bbf5cfd94244
FRANCISAnas <anas.francis@etu.univ-cotedazur.fr>
2021-09-24 20:31:54 +0200

    docker compose + database

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
--- dockerfile
+++ dockerfile
@@ -1,11 +1,11 @@
-FROM python:3.6.8-alpine
+FROM python:3.8
 
-WORKDIR .
+WORKDIR /usr/src/app
+
+RUN apt-get update && apt-get install -y netcat
 
 COPY . .
 
-RUN pip install -r requirements.txt
+RUN python3 -m pip install --no-cache-dir -r requirements.txt
 
-ENTRYPOINT [ "python3" ]
-
-CMD [ "app.py" ]
+ENTRYPOINT [ "python3", "app.py" ]
